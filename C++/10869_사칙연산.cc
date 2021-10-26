499c20dfa6339b5117627200c92d498f4e831d7b
junow2 <quilt257@gmail.com>
2021-10-26 22:01:39 +0900

    2021.10.26

---
+++ C++/10869_사칙연산.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+
+int main(void){
+  cin >> n >> m; 
+  cout << n + m << endl
+       << n - m << endl 
+       << n * m << endl
+       << n / m << endl
+       << n % m;
+
+}
