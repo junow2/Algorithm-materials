b4a7a15b5213539f47fe34ed2f5671d0ef6f54b5
junow2 <quilt257@gmail.com>
2022-01-08 03:00:34 +0900

    2022.01.08

---
+++ C++/3046_R2.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+int main(void) {
+  ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+  int r, s; cin >> r >> s; cout << s*2 - r;
+} 
