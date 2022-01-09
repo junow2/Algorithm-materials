b04074839851dc7e3b9a0e2a269c854a979724eb
junow2 <quilt257@gmail.com>
2022-01-09 22:20:20 +0900

    2022.01.09

---
+++ C++/15652_N과 M (4).cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+int arr[10]; 
+
+void nm(int n, int m, int k) {
+  if (k == m) {
+    for (int i=0; i < m; i++) cout << arr[i] << ' ';
+    cout << endl; return; 
+  }
+
+  for (int i=1; i <= n; i++) {
+    if (i < arr[k-1]) continue;
+    arr[k] = i;
+    nm(n, m, k+1);  
+  }
+}
+
+int main(void) {
+  ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+  int n, m; cin >> n >> m; 
+  nm(n, m, 0); 
+} 
---
+++ C++/15656_N과 M (7).cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+int arr[10], num[10]; 
+
+void nm(int n, int m, int k) {
+  if (k == m) {
+    for (int i=0; i < m; i++) cout << arr[i] << ' ';
+    cout << endl; return; 
+  }
+
+  for (int i=0; i < n; i++) {
+    arr[k] = num[i];    
+    nm(n, m, k+1);
+  }
+}
+
+int main(void) {
+  ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+  int n, m; cin >> n >> m; 
+  for (int i=0; i < n; i++)
+    cin >> num[i];
+  sort(num, num+n); 
+  nm(n, m, 0); 
+} 
