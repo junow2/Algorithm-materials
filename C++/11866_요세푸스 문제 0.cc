9205d7efe759195737a9378876dd5f751902d11d
junow2 <quilt257@gmail.com>
2022-01-14 18:11:03 +0900

    2022.01.14

---
+++ C++/11866_요세푸스 문제 0.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+  queue<int> q; 
+  int n, m; cin >> n >> m; 
+  
+  for (int i=1; i <= n; i++) q.push(i);
+
+  cout << '<';
+  while (!q.empty()) {
+    for (int i=0; i < m-1; i++) {
+      q.push(q.front()); q.pop();
+    }
+    cout << q.front(); q.pop(); 
+
+    if (!q.empty()) cout << ", ";
+  }
+  cout << '>';
+}
