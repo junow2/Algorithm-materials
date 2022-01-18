dacaaa85ee8881b28a6af1e17aec7d8d9a660e28
junow2 <quilt257@gmail.com>
2022-01-18 22:22:29 +0900

    2022.01.18

---
+++ C++/1550_16진수.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+int main(void) {
+  // ios::sync_with_stdio(false); cin.tie(NULL);
+
+  int n;   
+  scanf("%x", &n); printf("%d", n);
+}
