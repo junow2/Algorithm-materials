53517dd68ed098cd6d7c73873c520ac47342bd3f
junow2 <quilt257@gmail.com>
2024-03-26 17:33:06 +0900

    commit

---
+++ Baekjoon/C++/10926_!.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+string s; 
+
+int main(void) {
+  ios_base::sync_with_stdio(false); cin.tie(NULL);
+ 
+  cin >> s; cout << s << "??!";
+}
