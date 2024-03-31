ddb9216e895fda7cea93dac41190271e1e04d256
junow2 <42925796+junow2@users.noreply.github.com>
2024-03-31 15:51:16 +0900

    20240331

---
+++ Baekjoon/C++/15894_수학은 체육과목 입니다.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define ll long long
+ 
+ll N; 
+
+int main(void) {
+  ios::sync_with_stdio(false); cin.tie(NULL);
+
+  cin >> N; cout << 4 * N; 
+
+}
